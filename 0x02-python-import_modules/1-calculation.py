b4b4ad505fe0809a4039dc4ab8068fc6eee4dbcf
franklyn255 <franklinakunne025@gmail.com>
2023-05-11 14:26:02 +0000

    calculate

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,27 +1,10 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-	a = 10
-	b = 5
-	from calculator_1 import add
-
-	result = add(a, b)
-	print("{} + {} = {}".format(a, b, result))
-
-if __name__ == "__main__":
-	from calculator_1 import sub
-
-	result = sub(a, b)
-	print("{} + {} = {}".format(a, b, result))
-
-if __name__ == "__main__":
-	from calculator_1 import mul
-
-	result = mul(a, b)
-	print("{} + {} = {}".format(a, b, result))
-
-if __name__ == "__main__":
-	from calculator_1 import div
-
-	result = div(a, b)
-	print("{} + {} = {}".format(a, b, result))
+	from calculator_1 import add, sub, mul, div
+a = 10
+b = 5
+print("{} + {} = {}".format(a, b, add(a, b)))
+print("{} - {} = {}".format(a, b, sub(a, b)))
+print("{} * {} = {}".format(a, b, mul(a, b)))
+print("{} / {} = {}".format(a, b, div(a, b)))	
